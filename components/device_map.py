60c028e70aa1bc63d5941592e3696ad8638f5666
Jooc <1023578360@qq.com>
2021-07-14 17:17:53 +0800

    device label can move

--- components/device_map.py
+++ components/device_map.py
@@ -1,5 +1,5 @@
-from PyQt5.QtCore import QRect, QSize
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QSizePolicy, QLabel
+from PyQt5.QtCore import QRect, QSize, QPropertyAnimation, QEasingCurve, QPoint
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QSizePolicy, QLabel, QFrame
 from PyQt5.QtGui import QPixmap, QColor, QIcon, QImage
 
 import PyQt5.Qt
@@ -11,20 +11,31 @@ class DeviceMap(QWidget):
     def __init__(self, device_list):
         super().__init__()
 
-        # TODO: Animations
-
         container = QWidget(self)
         container.setFixedSize(global_pars.MAP_WIDTH, global_pars.MAP_HEIGHT)
 
         container.setStyleSheet('QWidget{background-color: #ffffff}')
         # background.setStyleSheet("background-color: yellow;")
 
+        button = QPushButton("Button")
+        button.clicked.connect(self.do_anim)
+
         layout = QVBoxLayout()
         layout.addWidget(container)
+        layout.addWidget(button)
 
         self.setLayout(layout)
 
-        label = QLabel(container)
+        self.label = QLabel(container)
         device0 = QPixmap(global_pars.BASE_DIR + "/assets/icons/toy_car.svg")
-        label.setPixmap(device0)
+        self.label.setPixmap(device0)
 
+        self.anim = None
+
+    def do_anim(self):
+        self.anim = QPropertyAnimation(self.label, b"pos")
+        self.anim.setDuration(100)
+        self.anim.setStartValue(self.label.pos())
+        self.anim.setEndValue(QPoint(self.label.pos().x() + 100, self.label.pos().y()+50))
+        self.label.rect().adjust(10, 10, 10, 10)
+        self.anim.start()
